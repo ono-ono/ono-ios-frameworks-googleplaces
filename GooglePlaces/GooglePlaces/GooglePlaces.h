40ad2fe88a198233bb129f06ade242033066e053
igor.tabacki <igor.tabacki@codetri.be>
2020-06-02 19:31:57 +0200

    Created initial project

---
+++ GooglePlaces/GooglePlaces/GooglePlaces.h
@@ -0,0 +1,19 @@
+//
+//  GooglePlaces.h
+//  GooglePlaces
+//
+//  Created by Igor Tabacki on 02/06/2020.
+//  Copyright © 2020 Igor Tabacki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GooglePlaces.
+FOUNDATION_EXPORT double GooglePlacesVersionNumber;
+
+//! Project version string for GooglePlaces.
+FOUNDATION_EXPORT const unsigned char GooglePlacesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GooglePlaces/PublicHeader.h>
+
+
